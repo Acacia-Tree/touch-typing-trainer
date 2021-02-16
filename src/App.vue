60c204fb1fcc73e8920dccbc10dbae6d1acb22db
Acacia-Tree <59014594+Acacia-Tree@users.noreply.github.com>
2021-02-16 14:48:27 +0300

    added boostrap library, deleted HelloWorld, added TypingArea component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TypingArea></TypingArea>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TypingArea from './components/TypingArea.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TypingArea
   }
 }
 </script>
---
+++ src/components/TypingArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text">
+  <div>
+    {{h1}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TypingArea',
+  data() {
+    return {
+      h1: "Тренажер слепой печати"
+    }
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
